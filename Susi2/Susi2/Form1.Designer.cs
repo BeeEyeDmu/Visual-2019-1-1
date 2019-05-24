33bc5fe9ec96161478cc995e8856bb67bd67ef63
BeeEye Dmu <bikang@konyang.ac.kr>
2019-05-24 10:45:20 +0900

    Add files via upload

---
+++ Susi2/Susi2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Susi2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 필수 디자이너 변수입니다.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 사용 중인 모든 리소스를 정리합니다.
+    /// </summary>
+    /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form 디자이너에서 생성한 코드
+
+    /// <summary>
+    /// 디자이너 지원에 필요한 메서드입니다. 
+    /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+      this.menuStrip1.Size = new System.Drawing.Size(700, 24);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // 파일ToolStripMenuItem
+      // 
+      this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.종료ToolStripMenuItem});
+      this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+      this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+      this.파일ToolStripMenuItem.Text = "파일";
+      // 
+      // 종료ToolStripMenuItem
+      // 
+      this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+      this.종료ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+      this.종료ToolStripMenuItem.Text = "종료";
+      this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+      // 
+      // panel1
+      // 
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.panel1.Location = new System.Drawing.Point(0, 24);
+      this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(700, 336);
+      this.panel1.TabIndex = 1;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(700, 360);
+      this.Controls.Add(this.panel1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.Move += new System.EventHandler(this.Form1_Move);
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+    private System.Windows.Forms.Panel panel1;
+  }
+}
+
---
+++ Susi2/Susi2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Susi2
+{
+  public partial class Form1 : Form
+  {
+    int mrg = 50;
+    int sqrSize = 30;
+    int gridSize = 100;
+    Graphics g;
+    Pen pen = new Pen(Color.Black);
+    Pen dashPen = new Pen(Color.Blue);
+
+    public Form1()
+    {
+      InitializeComponent();      
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+      base.OnPaint(e);
+
+      for (int i = 0; i < 5; i++)
+      {
+        g.DrawLine(pen, mrg, mrg + i * gridSize, mrg + 4 * gridSize, mrg + i*gridSize );  // 가로선
+        g.DrawLine(pen, mrg + i * gridSize, mrg, mrg + i * gridSize, mrg + 4 * gridSize);
+      }
+
+      g.DrawLine(dashPen, mrg, mrg, mrg + 4 * gridSize, mrg + 4 * gridSize);
+      g.DrawLine(dashPen, mrg, mrg + 4 * gridSize, mrg + 4 * gridSize, mrg);
+
+      int half = mrg + 2 * gridSize;
+      int end = mrg + 4 * gridSize;
+
+      g.DrawLine(dashPen, mrg, half, half, mrg);
+      g.DrawLine(dashPen, half, mrg, end, half);
+      g.DrawLine(dashPen, mrg, half, half, end);
+      g.DrawLine(dashPen, half, end, end, half);
+
+      for (int x = 1; x <= 3; x += 2)
+        for (int y = 1; y <= 3; y += 2)
+        {
+          Rectangle r = new Rectangle(mrg + x * gridSize - sqrSize / 2, 
+            mrg + y * gridSize - sqrSize / 2, sqrSize, sqrSize);
+          g.FillEllipse(new SolidBrush(Color.Red), r);
+        }
+      g.FillEllipse(new SolidBrush(Color.Red), 
+        new Rectangle(half - sqrSize/2, half - sqrSize/2, sqrSize, sqrSize));
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      this.Text = "학번 이름";
+      this.ClientSize = new Size(2 * mrg + 4 * gridSize, 2 * mrg + 4 * gridSize + menuStrip1.Height);
+      g = panel1.CreateGraphics();
+      panel1.BackColor = Color.Orange;
+    }
+
+    private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+
+    private void Form1_Move(object sender, EventArgs e)
+    {
+      Invalidate(); // Paint 이벤트를 강제로 만듭니다. 
+                    // ONPaint()호출
+    }
+  }
+}
