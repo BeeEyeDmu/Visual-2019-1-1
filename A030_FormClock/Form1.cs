dda26726c14be81de72c499a9a03d05f07c8ba92
BeeEye Dmu <bikang@konyang.ac.kr>
2019-05-31 17:03:45 +0900

    A030_FormClock 아날로그 디지털 메뉴 작성

--- A030_FormClock/Form1.cs
+++ A030_FormClock/Form1.cs
@@ -27,25 +27,24 @@ namespace A030_FormClock
     {
       InitializeComponent();
             
-      this.ClientSize = 
-        new Size(clientSize, clientSize+menuStrip1.Height);
+      this.ClientSize = new Size(clientSize, clientSize+menuStrip1.Height);
       this.Text = "Analog Clock";
       panel1.BackColor = Color.WhiteSmoke;
       this.Padding = new Padding(10);
-
       g = panel1.CreateGraphics();
 
-      if (dFlag == false)
-        aClockSetting();  // 아날로그클럭 세팅
-      else
-        dClockSetting();
+      aClockSetting();  // 아날로그 클럭 세팅
       TimerSetting();   // 타이머 세팅
     }
 
     private void dClockSetting()
-    {
-      panel1.Refresh();
+    {      
       dClock.Font = new Font("맑은 고딕", 20, FontStyle.Bold);
+      dClock.ForeColor = Color.RoyalBlue;
+      dClock.Width = clientSize;
+      dClock.Height = 40;
+      dClock.Location = new Point(0, (int)radius - dClock.Height/2);      
+      dClock.TextAlign = ContentAlignment.MiddleCenter;
       panel1.Controls.Add(dClock);
     }
 
@@ -130,6 +129,7 @@ namespace A030_FormClock
       secHand = (int)(radius * 0.65);
     }
 
+    /*
     private void T_Tick(object sender, EventArgs e)
     {
       panel1.Refresh();
@@ -167,18 +167,24 @@ namespace A030_FormClock
       g.DrawLine(new Pen(Color.Black), new Point(150, 150),
         new Point(x, y));
     }
+    */
 
     bool dFlag = false;
 
     private void 디지털ToolStripMenuItem_Click(object sender, EventArgs e)
     {
       dFlag = true;
-      dClock.Visible = true;
+      //dClock.Visible = true;
+      dClockSetting();
     }
 
     private void 아날로그ToolStripMenuItem_Click(object sender, EventArgs e)
     {
       dFlag = false;
+      if (dClock != null)
+        panel1.Controls.Remove(dClock);
+      panel1.Refresh();
+      //Console.WriteLine("Analog Click");
     }
   }
 }
